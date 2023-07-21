b7fae58e77062b8fb395241e9c95c9b69f21145f
“benoitregamey” <benoitregamey@hotmail.com>
2023-07-21 17:37:40 +0200

    initiate celery app

--- __init__.py
+++ __init__.py
@@ -1,14 +1,30 @@
 from flask import Flask
+from celery import Celery, Task
 
 def create_app():
     """Create Flask application."""
     app = Flask(__name__, instance_relative_config=False)
 
-    app.config.from_object('config.Config')  # configure app using the Config class defined in config.py
+    app.config.from_object('config.FlaskConfig')  # configure app using the Config class defined in config.py
 
     with app.app_context():
 
         from home.home import home_bp
         app.register_blueprint(home_bp)
 
+        celery_init_app(app)
+
         return app
+
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object('config.CeleryConfig')
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
 
-class Config:
+class FlaskConfig:
     ENV = 'development'
     DEBUG = True
     SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(24))
+
+class CeleryConfig:
+    broker_url="redis://localhost"
+    result_backend="redis://localhost"

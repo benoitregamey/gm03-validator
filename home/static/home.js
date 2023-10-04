f59f02d0670ba682c35dd43fbc336a7e4420306d
“benoitregamey” <benoitregamey@hotmail.com>
2023-10-04 13:37:03 +0200

    add errors in frontend app

--- home/static/home.js
+++ home/static/home.js
@@ -77,32 +77,61 @@ function read_md_results(results) {
     for (let i = 0; i < results.length; i++) {
 
         $("#md-results").append(
-            `<div class="row mx-1 mx-sm-2 mx-md-5 my-3 align-items-center flex-nowrap md-result">
-                <div class="col-8 col-sm-4 overflow-hidden">
-                    <p class="text-muted my-0 text-nowrap"></p>
-                </div>
-                <div class="col-sm-6 d-none d-sm-block overflow-hidden">
-                    <p class="text-muted my-0 text-nowrap"></p>
-                </div>
-                <div class="col-4 col-sm-2 overflow-hidden" style="min-width:100px">
-                    <p class="text-right my-0 text-nowrap"></p>
+            `<div class="mx-1 mx-sm-2 mx-md-5 my-3 md-result h-100">
+                <div class="row md-result-header mx-0 my-2 flex-nowrap align-items-center">
+                    <div class="col-8 col-sm-4 overflow-hidden">
+                        <p class="text-muted my-0 text-nowrap"></p>
+                    </div>
+                    <div class="col-sm-6 d-none d-sm-block overflow-hidden">
+                        <p class="text-muted my-0 text-nowrap"></p>
+                    </div>
+                    <div class="col-4 col-sm-2 overflow-hidden" style="min-width:100px">
+                        <p class="text-right my-0 text-nowrap"><</p>
+                    </div>
                 </div>
             </div>`
-        )   
+        );
 
-        $("#md-results .md-result").last().children().children("p").eq(0).text(results[i].uuid);
-        $("#md-results .md-result").last().children().children("p").eq(1).text(results[i].title);
+        $("#md-results .md-result").last().children().children().children("p").eq(0).text(results[i].uuid);
+        $("#md-results .md-result").last().children().children().children("p").eq(1).text(results[i].title);
 
         if (results[i].valid === "yes"){
-            $("#md-results .md-result").last().addClass("md-valid")
-            $("#md-results .md-result").last().children().children("p").eq(2).html(
+            $("#md-results .md-result").last().addClass("md-valid");
+            $("#md-results .md-result").last().children().children().children("p").eq(2).html(
                 '<i class="fa-solid fa-check"></i>&nbsp;valid'
             );
         } else {
-            $("#md-results .md-result").last().addClass("md-notvalid")
-            $("#md-results .md-result").last().children().children("p").eq(2).html(
+            $("#md-results .md-result").last().addClass("md-notvalid");
+            $("#md-results .md-result").last().children().children().children("p").eq(2).html(
                 '<i class="fa-solid fa-xmark"></i>&nbsp;not valid'
             );
+
+            // Add errors
+            $("#md-results .md-result").last().append(
+                `<div class="col md-result-error md-result-error-folded">
+                    <p class="mt-3">Errors</p>
+                    <hr>
+                </div>`
+            );
+
+            let errors = $("#md-results .md-result").last().find(".md-result-error");
+
+            for (let err_idx = 0; err_idx < results[i].errors.length; err_idx++) {
+
+                errors.append('<p class="text-break"></p>');
+                errors.children("p").last().text(results[i].errors[err_idx]);
+
+                // Add hr only if not last error
+                if (err_idx != results[i].errors.length -1){
+                    errors.append("<hr>");
+                }
+            }
         }
       }
+
+      $("#md-results .md-result.md-notvalid .md-result-header").click(function(){
+        $(this).parent().find(".md-result-error").toggleClass("md-result-error-folded md-result-error-unfolded");
+    });
+
   }
+
